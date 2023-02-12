28dee0bd0a18bebcb9a7a6ff4d0acc73bc1c3690
Dmytro Romantsov <romantsovmitya1@gmail.com>
2023-02-12 21:18:55 +0200

    Fixed issues

--- app/cafe.py
+++ app/cafe.py
@@ -1,16 +1,21 @@
 import datetime
-import errors
+from errors import VaccineError, NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
 
 
 class Visitor:
 
     def __init__(self, visitor: dict):
-        self.vaccine = visitor["vaccine"]
-        self.wearing_a_mask = visitor["wearing_a_mask"]
+        self.vaccine = visitor.get("vaccine")
+        self.wearing_a_mask = visitor.get("wearing_a_mask")
+        self.visiting_date = datetime.date.today()
 
     @property
     def vaccine_expiration_date(self) -> datetime.date:
-        return self.vaccine["expiration_date"]
+        return self.vaccine.get("expiration_date")
+
+    @property
+    def is_vaccine_date_expirated(self) -> bool:
+        return self.visiting_date > self.vaccine_expiration_date
 
 
 class Cafe:
@@ -18,21 +23,17 @@ class Cafe:
     def __init__(self, name):
         self.name = name
 
-    def visit_cafe(self, visitor_data: dict):
+    def visit_cafe(self, visitor_data: dict) -> str:
 
         visitor = Visitor(visitor_data)
 
-        date_today = datetime.date.today()
-
         if visitor.vaccine is None:
-            raise errors.NotVaccinatedError("You are not vaccinated")
+            raise NotVaccinatedError("You are not vaccinated")
 
-        elif date_today > visitor.vaccine_expiration_date:
-            raise errors.OutdatedVaccineError("Your vaccine certificate is out of date")
+        elif visitor.is_vaccine_date_expirated:
+            raise OutdatedVaccineError("Your vaccine certificate is out of date")
 
-        elif visitor.wearing_a_mask is False:
-            raise errors.NotWearingMaskError("You are not wearing a mask")
+        elif not visitor.wearing_a_mask:
+            raise NotWearingMaskError("You are not wearing a mask")
 
-        else:
-            print(f'Welcome to {self.name}')
-
+        return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,58 +1,14 @@
 import datetime
-import cafe, errors
+from .cafe import Cafe
+from errors import VaccineError, NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
 
-class Friends():
 
-    def __init__(self, friend: dict):
-        self.vaccine = friend["vaccine"]
-        self.wearing_a_mask = friend["wearing_a_mask"]
-
-    @property
-    def vaccine_expiration_date(self) -> datetime.date:
-        return self.vaccine["expiration_date"]
-
-    try:
-        def go_to_cafe(friends: list, cafe: cafe.Cafe):
-
-            date_today = datetime.date.today()
-
-            vaccine_flag = True
-            wearing_a_mask_flag = True
-
-            masks_to_buy = 0
-
-            for friend in friends:
-                if friend.vaccine is None or date_today >= friend.vaccine_expiration_date:
-                    vaccine_flag = False
-
-                if friend.wearing_a_mask is False:
-                    wearing_a_mask_flag = False
-                    masks_to_buy += 1
-
-            if vaccine_flag == False:
-                print("All friends should be vaccinated")
-
-            elif wearing_a_mask_flag == False:
-                print(f'Friends should buy {masks_to_buy} masks')
-
-            else:
-                print(f'Friends can go to {cafe.name}')
-
-    except errors.VaccineError:
-        pass
-    except errors.NotWearingMaskError:
-        pass
-
-Friends.go_to_cafe(
-        (
-            [
-                {
-                    "name": "Ivan",
-                    "vaccine": {
-                        "name": "Moderna",
-                        "expiration_date": datetime.date.today()
-                        + datetime.timedelta(days=4),
-                    },
-                    "wearing_a_mask": True,
-                },]
-            ), cafe.Cafe("KFC"))
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
+    masks_to_buy = 0
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except NotVaccinatedError:
+            return "All friends should be vaccinated"
+        ...
+    return "Friends can go to {}".format(cafe.name)
